577d6161acc19129a3e4ad95c2ec50b20ee4228f
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-06-19 19:37:08 -0700

    implement successful server post to api

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+var request = require('request');
+var token = require('./token.js');
 
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mongo');
@@ -8,12 +10,25 @@ var app = express();
 
 // UNCOMMENT FOR REACT
 app.use(express.static(__dirname + '/../react-client/dist'));
+// initialize bodyparser
+app.use(bodyParser());
 
-app.post('/shelters', function(req, res) {
+app.post('/shelters/search', function(req, res) {
 
+  let url = `http://api.petfinder.com/shelter.find?key=${token.token}&location=08901&format=json`;
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      
+      console.log(info);
+    }
+  } 
+  request(url, callback);
+  res.end('hello from successful server post');
 });
 
-app.get('/items', function (req, res) {
+app.get('/shelters', function (req, res) {
   items.selectAll(function(err, data) {
     if(err) {
       res.sendStatus(500);
